2ece6896272e9ff4995b03dd2826331835c8a171
samTime101 <srijanregmi44@gmail.com>
2025-04-04 11:55:12 +0545

    nayan changed

---
+++ Assets/Scripts/About.cs
@@ -0,0 +1,15 @@
+    using System.Collections;
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+
+    public class About : MonoBehaviour
+    {
+
+        public void MainMenu()
+        {
+                        SceneManager.LoadSceneAsync(0);
+
+        }
+
+
+    }
---
+++ Assets/Scripts/toAbout.cs
@@ -0,0 +1,14 @@
+    using System.Collections;
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+
+//test
+    public class toAbout : MonoBehaviour
+    {
+      //about function
+        public void About()
+        {
+            SceneManager.LoadSceneAsync(5);
+        }
+
+    }
